9211f34cb376d91a268924cc5f18e0206331c900
Blazeey-NotSure <57265550+Blazeey-NotSure@users.noreply.github.com>
2020-03-26 19:24:31 +0000

    please make changes to thos project

---
+++ project/app/src/main/java/com/example/groupproject/friendList.java
@@ -0,0 +1,14 @@
+package com.example.groupproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class friendList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.friendList);
+    }
+}
