1bc03dc2631f6480fe7547e2c0ac185e4533f740
Yaoting Wang <56686273+Midoriya-Gh0st@users.noreply.github.com>
2020-03-30 02:43:55 +0100

    Part for setting
    this step aims to create the basic layout of setting sessions.

---
+++ project/app/src/main/java/com/example/groupproject/fragment_setting_setting.java
@@ -0,0 +1,31 @@
+package com.example.groupproject;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fragment_setting_setting extends Fragment {
+
+
+    public fragment_setting_setting() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false);
+    }
+
+}
