1c50b5ac3ef8515ee19f40b22184b87c788d2c3b
Yaoting Wang <56686273+Midoriya-Gh0st@users.noreply.github.com>
2020-03-30 18:51:20 +0100

    sort jump of map

--- project/app/src/main/java/com/example/groupproject/fragment_setting_setting.java
+++ project/app/src/main/java/com/example/groupproject/fragment_setting_setting.java
@@ -1,6 +1,7 @@
 package com.example.groupproject;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -28,15 +29,42 @@ public class fragment_setting_setting extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_setting,null);
+
+        Button btn_friendlist_setting = view.findViewById(R.id.btn_friendlist_setting); //jump to friendlist
+        btn_friendlist_setting.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), friendlistActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn_channel_setting = view.findViewById(R.id.btn_channel_setting); //jump to channel
+        btn_channel_setting .setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), channel.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        //return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        Button btn_allpost_setting = view.findViewById(R.id.btn_allpost_setting);
+        Button btn_allpost_setting = view.findViewById(R.id.btn_allpost_setting); //allpost
         btn_allpost_setting.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,21 +72,16 @@ public class fragment_setting_setting extends Fragment {
             }
         });
 
-        Button btn_modify = view.findViewById(R.id.btn_modify);
+        Button btn_modify = view.findViewById(R.id.btn_modify); //modify
         btn_modify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Navigation.findNavController(v).navigate(R.id.action_setting2_to_modify);
             }
         });
-
-        Button btn_map = view.findViewById(R.id.btn_map);
-        btn_map.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Navigation.findNavController(v).navigate(R.id.action_setting_to_fragment_map_setting);
-            }
-        });
     }
+
+
+
 
 }
--- project/app/src/main/java/com/example/groupproject/map.java
+++ project/app/src/main/java/com/example/groupproject/map.java
@@ -1,8 +1,17 @@
 package com.example.groupproject;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class map extends AppCompatActivity {
 
@@ -10,5 +19,15 @@ public class map extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        Button button = findViewById(R.id.btn_back_map);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                map.super.onBackPressed();
+            }
+        });
     }
+
 }
