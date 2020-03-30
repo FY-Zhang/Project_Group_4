fa764535150640e18eff3b18938d3639eb0122a6
FY_Zhang <50323953+FY-Zhang@users.noreply.github.com>
2020-03-30 18:42:22 +0100

    edit

--- project/app/src/main/java/com/example/groupproject/channel.java
+++ project/app/src/main/java/com/example/groupproject/channel.java
@@ -2,7 +2,9 @@ package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class channel extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class channel extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_channel);
+    }
+
+    public void toPost(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, post.class);
+        startActivity(intent);
+    }
+    public void toFriendlist(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, friendlistActivity.class);
+        startActivity(intent);
+    }
+    public void toChannel(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, channel.class);
+        startActivity(intent);
+    }
+    public void toSetting(View view) {
+        Intent intent = new Intent();
+        intent.setClass(this, setting_main.class);
+        startActivity(intent);
     }
 }
--- project/app/src/main/java/com/example/groupproject/post.java
+++ project/app/src/main/java/com/example/groupproject/post.java
@@ -1,48 +1,88 @@
 package com.example.groupproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import static com.example.groupproject.friendlistActivity.FRIEND_NAME;
+
 public class post extends AppCompatActivity {
 
-    private ListView mListView;
-    private String[] posts;
+    Toolbar toolbar;
+    private ListView listview;
+    private ArrayList<String> list = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
-        mListView = (ListView) findViewById(R.id.post_list);
-        MyBaseAdapter mAdapter = new MyBaseAdapter();
-        mListView.setAdapter(mAdapter);
-        for(int i = 0;i < 10;i++)
-            posts[i] = "Post " + i + 1;
-    }
-    class MyBaseAdapter extends BaseAdapter {
-        @Override
-        public int getCount() {
-            return posts.length;
-        }
-        @Override
-        public Object getItem(int position) {
-            return posts[position];
-        }
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = View.inflate(post.this, R.layout.post_item, null);
-            TextView mTextView = (TextView) view.findViewById(R.id.item_text);
-            mTextView.setText(posts[position]);
-            return view;
-        }
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        listview = (ListView)findViewById(R.id.item);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_expandable_list_item_1,
+                getData());
+        listview.setAdapter(adapter);
+
+        /* 待修改 */
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position,
+                                    long id) {
+
+                String friendName = (String)parent.getItemAtPosition(position);
+                Intent intent = new Intent();
+                intent.putExtra(FRIEND_NAME, friendName);
+                intent.setClass(friendlistActivity.this,chat_nav.class);
+                startActivity(intent);
+            }
+
+        });
+    }
+
+    private ArrayList<String> getData()
+    {
+        list.add("Post_1");
+        list.add("Post_2");
+        list.add("Post_3");
+        list.add("Post_4");
+        list.add("Post_5");
+        list.add("Post_1");
+        list.add("Post_2");
+        list.add("Post_3");
+        list.add("Post_4");
+        list.add("Post_5");
+        list.add("Post_1");
+        list.add("Post_2");
+        list.add("Post_3");
+        list.add("Post_4");
+        list.add("Post_5");
+        return list;
+    }
+    public void toBack(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, channel.class);
+        startActivity(intent);
+    }
+    public void toNewPost(View view){
+        Intent intent = new Intent();
+        intent.setClass(this, new_post.class);
+        startActivity(intent);
     }
 }
