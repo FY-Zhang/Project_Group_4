06869b60e0d1f25a5b8352c55866b31ba73fdd38
Yaoting Wang <56686273+Midoriya-Gh0st@users.noreply.github.com>
2020-03-31 10:26:16 +0100

    sort display func

--- project/app/src/main/java/com/example/groupproject/fragment_setting_setting.java
+++ project/app/src/main/java/com/example/groupproject/fragment_setting_setting.java
@@ -13,6 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 
 /**
@@ -31,6 +38,8 @@ public class fragment_setting_setting extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_setting,null);
+
+        showInfo_setting(view);
 
         Button btn_friendlist_setting = view.findViewById(R.id.btn_friendlist_setting); //jump to friendlist
         btn_friendlist_setting.setOnClickListener(new View.OnClickListener()
@@ -81,7 +90,37 @@ public class fragment_setting_setting extends Fragment {
         });
     }
 
+    public void showInfo_setting(final View view){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document : task.getResult()) { //show info
+                                TextView tv1 = view.findViewById(R.id.dis_username);
+                                tv1.setText(document.getString("username"));
+
+                                TextView tv2 = view.findViewById(R.id.dis_phone);
+                                tv2.setText(document.getString("phone"));
+
+                                TextView tv3 = view.findViewById(R.id.dis_email);
+                                tv3.setText(document.getString("email"));
+
+                                TextView tv4 = view.findViewById(R.id.dis_gender);
+                                tv4.setText(document.getString("gender"));
+
+                                TextView tv5 = view.findViewById(R.id.dis_age);
+                                tv5.setText(document.getString("age"));
+                                System.out.println(document.getString("username"));
+                                System.out.println(document.getData());
+                            }
+                        }
+                    }
+                });
+    }
 
 
 }
--- project/app/src/main/java/com/example/groupproject/setting_main.java
+++ project/app/src/main/java/com/example/groupproject/setting_main.java
@@ -1,15 +1,19 @@
 package com.example.groupproject;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -21,44 +25,7 @@ public class setting_main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
 
-        showInfo_setting();
     }
 
-    public void showInfo_setting(){
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()) {
-                            for(QueryDocumentSnapshot document : task.getResult()) { //show info
-                                TextView tv1 = findViewById(R.id.dis_username);
-                                tv1.setText(document.getString("username"));
-
-                                TextView tv2 = findViewById(R.id.dis_phone);
-                                tv2.setText(document.getString("phone"));
-
-                                TextView tv3 = findViewById(R.id.dis_email);
-                                tv3.setText(document.getString("email"));
-
-                                TextView tv4 = findViewById(R.id.dis_gender);
-                                tv4.setText(document.getString("gender"));
-
-                                TextView tv5 = findViewById(R.id.dis_age);
-                                tv5.setText(document.getString("age"));
-                            }
-                        }
-                    }
-                });
-    }
-
-    public void onClickStoreData_setting(View view) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        CollectionReference username = db.collection("username");
-
-        
-    }
 }
