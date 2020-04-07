1ad13c1d5fe2670e85285322d6d3763498451254
Yaoting Wang <56686273+Midoriya-Gh0st@users.noreply.github.com>
2020-04-07 14:44:36 +0100

    resolve mainifest

--- project/app/src/main/java/com/example/groupproject/map_main.java
+++ project/app/src/main/java/com/example/groupproject/map_main.java
@@ -3,6 +3,7 @@ package com.example.groupproject;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -11,12 +12,60 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.GroundOverlay;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 public class map_main extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private static final LatLng UL = new LatLng(52.673829, -8.572475);
+    private static final LatLng Beijing = new LatLng(39.901389, 116.4214707);
+    private static final LatLng Dublin = new LatLng(53.331876, -6.257069);
+    private static final LatLng Washington = new LatLng(38.907757, -77.035599);
+    private static final LatLng Sydney = new LatLng(-34, 151);
+
+    private static class CustomTag {
+        private final String description;
+        private int clickCount;
+
+        public CustomTag(String description) {
+            this.description = description;
+            clickCount = 0;
+        }
+
+        public void incrementClickCount() {
+            clickCount++;
+        }
+
+        @Override
+        public String toString() {
+            return "The " + description + " has been clicked " + clickCount + " times.";
+        }
+    }
+
+    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+    private static final LatLng DARWIN = new LatLng(-12.425892, 130.86327);
+    private static final LatLng HOBART = new LatLng(-42.8823388, 147.311042);
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+    private Circle mAdelaideCircle;
+    private GroundOverlay mSydneyGroundOverlay;
+    private Marker mHobartMarker;
+    private Polygon mDarwinPolygon;
+    private Polyline mPolyline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +90,28 @@ public class map_main extends FragmentActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng lero = new LatLng(52.674423, -8.576994); //在这里读取firebase,然后写入
-        mMap.addMarker(new MarkerOptions().position(lero).title("Lero"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(lero));
+        addOfficialMarkers();
+        showIreland(null);
+    }
+
+    private void addOfficialMarkers() { // official check points
+        mMap.addMarker(new MarkerOptions().position(UL).title("University of Limerick."));
+        mMap.addMarker(new MarkerOptions().position(Beijing).title("Beijing, Capital of China."));
+        mMap.addMarker(new MarkerOptions().position(Dublin).title("Dublin, Capital of Ireland."));
+        mMap.addMarker(new MarkerOptions().position(Washington).title("Washington, Capital of USA."));
+        mMap.addMarker(new MarkerOptions().position(Sydney).title("Sydney, Capital of Sydney."));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(Dublin));
+    }
+
+    public void showIreland(View v) {
+        if (mMap == null)
+            return;
+        // Create bounds that include all locations of the map
+        LatLngBounds.Builder bounds = LatLngBounds.builder()
+                .include(Dublin)
+                .include(UL);
+        // Move camera to show all markers and locations
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds.build(), 20));
     }
 
     public void map_GetCurrentLocation(View view) {
@@ -65,6 +133,48 @@ public class map_main extends FragmentActivity implements OnMapReadyCallback {
         Intent intent = new Intent();
         intent.setClass(this, pointsList.class);
         startActivity(intent); */
+    }
+
+    private void addObjectsToMap() {
+        // A circle centered on Adelaide.
+        mAdelaideCircle = mMap.addCircle(new CircleOptions()
+                .center(ADELAIDE)
+                .radius(500000)
+                .fillColor(Color.argb(150, 66, 173, 244))
+                .strokeColor(Color.rgb(66, 173, 244))
+                .clickable(true));
+        mAdelaideCircle.setTag(new CustomTag("Adelaide circle"));
+
+        // A ground overlay at Sydney.
+        mSydneyGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+                .image(BitmapDescriptorFactory.fromResource(R.drawable.icon_camera))
+                .position(SYDNEY, 700000)
+                .clickable(true));
+        mSydneyGroundOverlay.setTag(new CustomTag("Sydney ground overlay"));
+
+        // A marker at Hobart.
+        mHobartMarker = mMap.addMarker(new MarkerOptions().position(HOBART));
+        mHobartMarker.setTag(new CustomTag("Hobart marker"));
+
+        // A polygon centered at Darwin.
+        mDarwinPolygon = mMap.addPolygon(new PolygonOptions()
+                .add(
+                        new LatLng(DARWIN.latitude + 3, DARWIN.longitude - 3),
+                        new LatLng(DARWIN.latitude + 3, DARWIN.longitude + 3),
+                        new LatLng(DARWIN.latitude - 3, DARWIN.longitude + 3),
+                        new LatLng(DARWIN.latitude - 3, DARWIN.longitude - 3))
+                .fillColor(Color.argb(150, 34, 173, 24))
+                .strokeColor(Color.rgb(34, 173, 24))
+                .clickable(true));
+        mDarwinPolygon.setTag(new CustomTag("Darwin polygon"));
+
+        // A polyline from Perth to Brisbane.
+        mPolyline = mMap.addPolyline(new PolylineOptions()
+                .add(PERTH, BRISBANE)
+                .color(Color.rgb(103, 24, 173))
+                .width(30)
+                .clickable(true));
+        mPolyline.setTag(new CustomTag("Perth to Brisbane polyline"));
     }
 
 }
