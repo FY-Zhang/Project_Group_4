336e450110992fcb12b80050b46d1c60edbe272a
Blazeey-NotSure <57265550+Blazeey-NotSure@users.noreply.github.com>
2020-04-08 21:44:02 +0100

    Camera function(in chat window)added yet not completed

--- project/app/src/main/java/com/example/groupproject/chat_camera.java
+++ project/app/src/main/java/com/example/groupproject/chat_camera.java
@@ -1,14 +1,41 @@
 package com.example.groupproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageView;
 
 public class chat_camera extends AppCompatActivity {
 
+    private ImageView imgView;
+    private static final int REQUEST_IMAGE_CAPTURE = 789;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat_camera);
+
+        imgView = findViewById(R.id.picture);
+
+        Intent cameraUse = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if(cameraUse.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(cameraUse, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE ){
+
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap)extras.get("data");
+            imgView.setImageBitmap(imgBitmap);
+        }
     }
 }
