ca5661b8bdb172eb4c6fced78d98f5dd2e2421ff
Blazeey-NotSure <57265550+Blazeey-NotSure@users.noreply.github.com>
2020-04-12 15:13:08 +0100

    Few bugs fixed

--- project/app/src/main/java/com/example/groupproject/profile.java
+++ project/app/src/main/java/com/example/groupproject/profile.java
@@ -61,52 +61,71 @@ public class profile extends AppCompatActivity {
         information = findViewById(R.id.information);
         infoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1);
 
-        if(type.equals("friends")){
-            for(int i=0; i<userFriends.size(); i++){
-                if(userFriends.get(i).get("UID").equals(UID)){
-                    user_email = userFriends.get(i).get("email").toString();
-                    user_name = userFriends.get(i).get("username").toString();
-                    user_gender = userFriends.get(i).get("gender").toString();
-                    showGender = (boolean)userFriends.get(i).get("display");
-                    break;
-                }
-            }
-            setInfo();
-            infoAdapter.addAll(content);
-            information.setAdapter(infoAdapter);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users").document(UID)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        user_email = documentSnapshot.getString("email");
+                        user_name = documentSnapshot.getString("username");
+                        user_gender = documentSnapshot.getString("gender");
+                        showGender = documentSnapshot.getBoolean("display");
 
-        }else if(type.equals("request") || type.equals("unknown")){
-            if(type.equals("unknown")){
-                button1.setText("Send Friend Request");
-            }else {
-                button1.setText("Accept Request");
-            }
+                        setInfo();
+                        infoAdapter.addAll(content);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
 
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
-            db.collection("users").document(UID)
-                    .get()
-                    .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                        @Override
-                        public void onSuccess(DocumentSnapshot documentSnapshot) {
-                            user_email = documentSnapshot.getString("email");
-                            user_name = documentSnapshot.getString("username");
-                            user_gender = documentSnapshot.getString("gender");
-                            showGender = documentSnapshot.getBoolean("display");
+                    }
+                });
 
-                            setInfo();
-                            infoAdapter.addAll(content);
-                        }
-                    })
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
+        information.setAdapter(infoAdapter);
 
-                        }
-                    });
-            information.setAdapter(infoAdapter);
+        if(type.equals("friends") || userFriendsID.contains(UID)){
+
+        }else if(type.equals("request")){
+            button1.setText("Accept Request");
+
+        }else if(type.equals("unknown")) {
+            button1.setText("Send Friend Request");
         }
 
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (type){
+                    case "friends":
+                        Intent intent = new Intent();
+                        intent.putExtra("friend_id", UID);
+                        intent.putExtra("friend_name", user_name);
+                        intent.setClass(profile.this, chat_nav.class);
+                        startActivity(intent);
+                        break;
+                    case "request":
+                        addAsFriend();
+                        button1.setText("SEND MESSAGE");
+                        break;
+                    case "unknown":
+                        sendRequest();
+                        break;
+                }
 
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(profile.this, post_friend.class);
+                intent.putExtra("friendId", UID);
+                startActivity(intent);
+            }
+        });
     }
     private void setInfo(){
 
@@ -128,33 +147,13 @@ public class profile extends AppCompatActivity {
         db.collection("users")
                 .document(UID)
                 .update("friends", FieldValue.arrayUnion(userID));
+
+        userFriendsID.add(UID);
     }
     private void sendRequest(){
         final FirebaseFirestore db = FirebaseFirestore.getInstance();
         db.collection("users")
                 .document(UID)
                 .update("notifications", FieldValue.arrayUnion(userID));
-    }
-    public void showPost(View view){
-        Intent intent = new Intent();
-        intent.putExtra("friendId", UID);
-        startActivity(intent);
-    }
-    public void button1Function(View view){
-        Intent intent = new Intent();
-        switch (type){
-            case "friends":
-                intent.putExtra("friend_id", UID);
-                intent.putExtra("friend_name", user_name);
-                startActivity(intent);
-                break;
-            case "request":
-                addAsFriend();
-                button1.setText("SEND MESSAGE");
-                break;
-            case "unknown":
-                sendRequest();
-                break;
-        }
     }
 }
